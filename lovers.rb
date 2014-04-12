5f3cf1f758bbc17e7d4ef9aeb41db590b58dc887
hilohiro <hilohiro+bitbucket@gmail.com>
2014-04-13 01:05:25 +0900

    Refactored.

--- lovers.rb
+++ lovers.rb
@@ -1,22 +1,26 @@
-def parseAttendant(line)
+def parse_attendant(line)
   name, expects = line.split(':', 2)
   [name, expects.split(',')]
 end
 
-def simple_match(mens, ladies)
-  free = mens.keys + ladies.keys
-  weight(mens, ladies).sort_by { |couple, priority| priority }.select { |(m, l), _|
+def simple_match(weighted)
+  free = weighted.keys.flatten.uniq
+  weighted.sort_by(&:last).map(&:first).select do |m, l|
     free = free - [m, l] if free.include?(m) && free.include?(l)
-  }.map { |couple, _| couple }
+  end
 end
 
-def weight(mens, ladies)
-  mens.each_with_object({}) do |(m, exp), matrix|
+def weight(men, ladies)
+  men.each_with_object({}) do |(m, exp), matrix|
     exp.each_with_index do |l, i|
       expected = ladies.fetch(l,[]).index(m)
       matrix[[m, l]] = [i, expected].max unless expected.nil?
     end
   end
+end
+
+def show_couples(couples)
+  puts couples.map { |couple| couple.join('-') }.join("\n")
 end
 
 ATTENDANTS = <<__ATT__
@@ -34,5 +38,5 @@ e:C,A,E
 f:D,B,A
 __ATT__
 
-mens, ladies = ATTENDANTS.each_line.map { |line| parseAttendant(line.chop) }.partition { |n, _| ('A'..'Z') === n[0] }.map { |attendants| Hash[attendants] }
-p simple_match(mens, ladies)
+men, ladies = ATTENDANTS.each_line.map { |line| parse_attendant(line.chop) }.partition { |n, _| ('A'..'Z') === n[0] }.map { |attendants| Hash[attendants] }
+puts simple_match(weight(men, ladies)).map { |couple| couple.join('-') }.join("\n")
